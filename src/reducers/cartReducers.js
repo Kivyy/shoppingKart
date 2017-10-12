149fd29141212c533ae950cf2b3d593e7e8fd46e
Kiv Lo <kivlokw@gmail.com>
2017-10-12 12:45:08 -0400

    add a total amount reducers and map to cart component

--- src/reducers/cartReducers.js
+++ src/reducers/cartReducers.js
@@ -3,7 +3,7 @@
 export function cartReducers(state = {cart: []}, action){
   switch(action.type){
     case('ADD_TO_CART'):
-    return {cart: [...state,...action.payload]}
+    return {cart: [...state,...action.payload,] , totalAmount: totals(action.payload).amount , totalQty: totals(action.payload).qty}
     break
 
     case('UPDATE_CART'):
@@ -18,15 +18,31 @@ export function cartReducers(state = {cart: []}, action){
     }
 
     let cartUpdate =  [...currentCartToUpdate.slice(0,indexToUpdate), newCartToUpdate, ...currentCartToUpdate.slice(indexToUpdate + 1 )]
-    return {...state,cart: cartUpdate}
+    return {...state,cart: cartUpdate, totalAmount: totals(cartUpdate).amount,totalQty: totals(cartUpdate).qty}
     break
 
     case('DELETE_CART_ITEM'):
-    return {cart: [...state,...action.payload]}
+    return {cart: [...state,...action.payload],totalAmount: totals(action.payload).amount,totalQty: totals(action.payload).qty}
     break
 
 
   }
 
   return state
+}
+
+export function totals(payloadArr){
+  const totalAmount = payloadArr.map((cartArr) => {
+    return cartArr.price * cartArr.quantity;
+  }).reduce((a,b) => {
+    return a + b;
+  },0)
+
+  const totalQty = payloadArr.map((qty) => {
+    return qty.quantity;
+  }).reduce((a,b) => {
+    return a + b
+  },0)
+
+  return {amount: totalAmount.toFixed(2), qty: totalQty}
 }
